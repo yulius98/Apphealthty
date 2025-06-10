61ecdb50d3398a44b912d452577b7f657d4b7480
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2025-06-11 00:08:30 +0700

    first commit

---
+++ resources/views/Cek_Gratis.blade.php
@@ -0,0 +1,3 @@
+<x-leandingpage>
+    <x-nav-bar-home />
+</x-leandingpage>
---
+++ resources/views/Home.blade.php
@@ -0,0 +1,75 @@
+<x-leandingpage>
+  <!-- Navigation bar-->
+  <x-nav-bar-home />
+      
+  <!-- Mashead header-->
+  <header class="masthead">
+    <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+          <div class="col-lg-6">
+              <!-- Mashead text and app badges-->
+              <div class="mb-5 mb-lg-0 text-center text-lg-start">
+                  <h1 class="display-1 lh-1 mb-3">Saatnya Detox</h1>
+                  <p class="lead fw-normal text-muted mb-5">Puasa dengan 8 botol jus bantu Berat Badan ideal, Maag GERD aman, Mens lancar dan Kolesterol turun.</p>
+              </div>
+          </div>
+          <div class="col-lg-6">
+          <!-- Masthead device mockup feature-->
+          <div class="masthead-device-mockup">
+            <div class="device-wrapper">
+              <div class="device" data-device="iPhoneX" data-orientation="portrait" data-color="black">
+                  <div class="screen bg-transparent">
+                      <!-- PUT CONTENTS HERE:-->
+                      <!-- * * This can be a video, image, or just about anything else.-->
+                      <!-- * * Set the max width of your media to 100% and the height to-->
+                      <!-- * * 100% like the demo example below.-->
+                      <video muted="muted" autoplay="" loop="" style="max-width: 100%; height: 100%"><source src="assets/img/healty.mp4" type="video/mp4" /></video>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </header>
+      
+  <section>
+    <!-- Produk -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12 ps-md-5 ps-4"> <!-- padding start agar bergeser ke kanan -->
+          <h2 class="fw-semibold fs-24 sm-fs-48 mb-3">
+            <span>Best Seller</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <!-- Tentang Detox -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12 ps-md-5 ps-4"> <!-- padding start agar bergeser ke kanan -->
+          <h2 class="fw-semibold fs-24 sm-fs-48 mb-3">
+            <span>Apa itu Detox ?</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
+
+      
+  <footer class="text-center py-5">
+      <div class="container px-5">
+          <div class="text-black small">
+            <div class="mb-2">Nama Perusahaan</div>  
+            <div class="mb-2">Alamat</div>
+            <div class="mb-2">No. Telp</div>
+            <div class="mb-2">Social Media</div>
+            
+          </div>
+      </div>
+  </footer>
+  
+</x-leandingpage>
+
